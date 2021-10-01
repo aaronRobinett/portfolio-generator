c52cdbd1954f85e309b0ff97f89c3f1f1bd3009c
Aaron <aaronrobinett@gmail.com>
2021-10-01 15:04:46 -0700

    finished project

---
+++ utils/generate-site.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            //if error reject promise 
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            // if no error resolve
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
+
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'Style sheet copied successfully!'
+            });
+        });
+    });
+};
+
+module.exports = {writeFile, copyFile};
